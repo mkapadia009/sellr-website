78934cc2c63ae036e69c69f006d2b55938c441c4
mkapadia93 <manan@traverseinformatics.com>
2025-07-29 13:28:09 +0530

    Removed LazyImage from SellrThemesSection

--- src/components/SellrThemesSection.tsx
+++ src/components/SellrThemesSection.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
-import LazyImage from './LazyImage';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { ArrowRight } from 'lucide-react';
 
