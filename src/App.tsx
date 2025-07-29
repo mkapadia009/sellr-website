a55be92d9b02062dcaba523ea9ccf002d468d183
mkapadia93 <manan@traverseinformatics.com>
2025-07-29 13:13:12 +0530

    Removed old pages and news data

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { HashProvider } from "./contexts/HashContext";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import KundenservicePage from "./pages/KundenservicePage";
-import HrItSupportPage from "./pages/HrItSupportPage";
-import ECommercePage from "./pages/ECommercePage";
-import FinanzdienstleistungenPage from "./pages/FinanzdienstleistungenPage";
-import BehoerdenPage from "./pages/BehoerdenPage";
-import BildungPage from "./pages/BildungPage";
-import NewsOverviewPage from "./pages/NewsOverviewPage";
-import NewsDetailPage from "./pages/NewsDetailPage";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
 const queryClient = new QueryClient();
@@ -33,24 +25,7 @@ const App = () => (
             <HashProvider>
               <Routes>
                 <Route path="/" element={<Index />} />
-                <Route path="/direkter-zugriff-auf-ausgewaehlte-microsoft-365-dokumente-ueber-typo3" element={<KundenservicePage />} />
-                <Route path="/zentrale-datenspeicherung-mit-sptools" element={<HrItSupportPage />} />
-                <Route path="/sptools-terminbuchungsloesung" element={<ECommercePage />} />
-                <Route path="/workflow-automatisierung" element={<FinanzdienstleistungenPage />} />
-                <Route path="/sptools-echtzeitkommunikation" element={<BehoerdenPage />} />
-                <Route path="/sptools-bewerbermanagement" element={<BildungPage />} />
                 
-                {/* News-Seiten */}
-                <Route path="/news" element={<NewsOverviewPage />} />
-                <Route path="/news/:id" element={<NewsDetailPage />} />
-                
-                {/* Legacy routes with redirects */}
-                <Route path="/kundenservice" element={<Navigate to="/direkter-zugriff-auf-ausgewaehlte-microsoft-365-dokumente-ueber-typo3" replace />} />
-                <Route path="/hr-it-support" element={<Navigate to="/zentrale-datenspeicherung-mit-sptools" replace />} />
-                <Route path="/ecommerce" element={<Navigate to="/sptools-terminbuchungsloesung" replace />} />
-                <Route path="/finanzdienstleistungen" element={<Navigate to="/workflow-automatisierung" replace />} />
-                <Route path="/behoerden" element={<Navigate to="/sptools-echtzeitkommunikation" replace />} />
-                <Route path="/bildung" element={<Navigate to="/sptools-bewerbermanagement" replace />} />
                 
                 {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                 <Route path="*" element={<NotFound />} />
--- src/config/contactConfig.ts
+++
@@ -1,7 +0,0 @@
-
-// Contact section configuration
-export const contactConfig = {
-  backgroundImage: 'https://raw.githubusercontent.com/stone1971grey/projekt-bilder/main/Hero_kontakt.jpg',
-  overlayGradient: 'rgba(0, 0, 0, 0.1)', // Updated to even lighter overlay
-  sectionHeight: '700px',
-};
